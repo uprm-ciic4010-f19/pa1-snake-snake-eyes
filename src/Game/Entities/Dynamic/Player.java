5151ba9ac48e424fcc8848d4e4a61bf0e02d4cbb
Jan A. Lebron Lebron <jan.lebron2@upr.edu>
2019-09-08 16:30:01 -0400

    Added teleport to the opposite side

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -79,28 +79,28 @@ public class Player {
         switch (direction){
             case "Left":
                 if(xCoord==0){
-                    kill();
+                	xCoord=handler.getWorld().GridWidthHeightPixelCount-1;
                 }else{
                     xCoord--;
                 }
                 break;
             case "Right":
                 if(xCoord==handler.getWorld().GridWidthHeightPixelCount-1){
-                    kill();
+                    xCoord=1;
                 }else{
                     xCoord++;
                 }
                 break;
             case "Up":
                 if(yCoord==0){
-                    kill();
+                	yCoord=handler.getWorld().GridWidthHeightPixelCount-1;
                 }else{
                     yCoord--;
                 }
                 break;
             case "Down":
                 if(yCoord==handler.getWorld().GridWidthHeightPixelCount-1){
-                    kill();
+                    yCoord=1;
                 }else{
                     yCoord++;
                 }
